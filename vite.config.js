93f7beaa262eec6b7439f5bd7edc40fa55785feb
dhaniarfebrin <dhaniarfebrinwahyudi@gmail.com>
2023-06-30 01:26:18 +0700

    adding dist

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/redcom-client/',
+  base: '/redcom/',
   plugins: [
     vue(),
     vueJsx(),
