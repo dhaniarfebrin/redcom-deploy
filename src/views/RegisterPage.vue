49f4183b861a347d50317d77c4b9cec526fc1427
dhaniarfebrin <dhaniarfebrinwahyudi@gmail.com>
2023-07-04 00:27:09 +0700

    feat: register

--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -1,5 +1,6 @@
 <script setup>
-import NavBar from '../components/NavBar.vue';
+// import NavBar from '../components/NavBar.vue';
+import axios from 'axios';
 </script>
 
 <script>
@@ -7,7 +8,9 @@ export default {
     data() {
         return {
             passwordFieldType: "password",
-            classShowHide: "bi bi-eye-slash"
+            classShowHide: "bi bi-eye-slash",
+            dataRegister: {},
+            messageError: ''
         }
     },
     methods: {
@@ -16,9 +19,23 @@ export default {
                 this.passwordFieldType = "password"
                 this.classShowHide = "bi bi-eye-slash"
             } else {
-                this.passwordFieldType =  "text"
+                this.passwordFieldType = "text"
                 this.classShowHide = "bi bi-eye"
             }
+        },
+        register() {
+            axios.post(`${import.meta.env.VITE_APP_ROOT_API}api/auth/signup`, this.dataRegister)
+                .then(response => {
+                    this.$router.push({ path: '/login' })
+                })
+                .catch(err => {
+                    if (err.response.status === 401) {
+                        this.messageError = err.response.data.message
+                        console.log("Error fetching: ", err.response.data.message)
+                    } else {
+                        console.log("Error fetching", err.response.status)
+                    }
+                })
         }
     },
     created() {
@@ -34,22 +51,37 @@ export default {
     <div class="container mt-auto h-100">
         <div class="d-flex flex-column justify-content-center align-items-center">
             <h2 class="fw-bold">Register</h2>
-            <p class="text-center">Tell us about yourself. We need<br>this information to ensure Redcom as a safe place to learn</p>
+            <p class="text-center">Tell us about yourself. We need<br>this information to ensure Redcom as a safe place to
+                learn</p>
             <div class="form-register w-25 mt-4">
-                <form action="">
+
+                <!-- alert -->
+                <div class="alert alert-danger alert-dismissible d-flex align-items-center" role="alert"
+                    v-if="messageError">
+                    <i class="bi bi-exclamation-triangle-fill me-2"></i>
+                    <div class="m-0">
+                        {{ messageError }}
+                    </div>
+                </div>
+
+                <form action="#" v-on:submit.prevent="register">
                     <div class="form-group">
-                        <label for="" class="form-label">Full Name</label>
-                        <input type="text" class="form-control rounded-pill bg-body-secondary">
+                        <label for="" class="form-label">Username</label>
+                        <input type="text" class="form-control rounded-pill bg-body-secondary"
+                            v-model="dataRegister.username" required>
                     </div>
                     <div class="form-group mt-3">
                         <label for="" class="form-label">Email</label>
-                        <input type="email" class="form-control rounded-pill bg-body-secondary">
+                        <input type="email" class="form-control rounded-pill bg-body-secondary" v-model="dataRegister.email"
+                            required>
                     </div>
                     <div class="form-group mt-3">
                         <label for="" class="form-label">Password</label>
                         <div class="input-group mb-3">
-                            <input :type="passwordFieldType" class="form-control rounded-start-pill bg-body-secondary">
-                            <button type="button" class="bg-body-secondary input-group-text rounded-end-pill" @click="togglePasswordFieldType">
+                            <input :type="passwordFieldType" class="form-control rounded-start-pill bg-body-secondary"
+                                v-model="dataRegister.password" required>
+                            <button type="button" class="bg-body-secondary input-group-text rounded-end-pill"
+                                @click="togglePasswordFieldType">
                                 <i :class="classShowHide"></i>
                             </button>
                         </div>
@@ -57,7 +89,7 @@ export default {
                     <button type="submit" class="btn btn-dark w-100 rounded-pill mt-4 py-2">Register</button>
                 </form>
             </div>
-            <span class="mt-4">Already have an account? <router-link to="/login" class="text-danger">Login Here</router-link></span>
-        </div>
+        <span class="mt-4">Already have an account? <router-link to="/login" class="text-danger">Login
+                Here</router-link></span>
     </div>
-</template>
+</div></template>
