57f98de30b338d129da301f791bab59243b27847
dhaniarfebrin <dhaniarfebrinwahyudi@gmail.com>
2023-07-06 13:32:55 +0700

    view: edit profile page

--- src/views/EditProfilePage.vue
+++ src/views/EditProfilePage.vue
@@ -8,27 +8,46 @@ export default {
     name: 'EditProfilePage',
     data() {
         return {
-            userData: {}
+            userData: {},
+            configRequest: {},
+            messageError: ''
         }
     },
     methods: {
         setUserData(data) {
             this.userData = data
+        },
+        editUser() {
+            axios.put(`${import.meta.env.VITE_APP_ROOT_API}api/auth/edit`, this.userData, this.configRequest)
+                .then(response => {
+                    this.$router.push({ path: '/profile' })
+                })
+                .catch(err => {
+                    if (err.response.status === 401) {
+                        this.messageError = err.response.data.msg
+                        console.log("Error fetching: ", err.response.data.msg)
+                    } else {
+                        console.log("Error fetching", err.response)
+                    }
+                })
+        },
+        goBack() {
+            window.history.back()
         }
     },
     created() {
-        const config = {
+        this.configRequest = {
             headers: {
                 Authorization: `Bearer ${localStorage.getItem("user")}`
             }
         }
 
-        axios.get(`${import.meta.env.VITE_APP_ROOT_API}api/auth/data`, config)
+        axios.get(`${import.meta.env.VITE_APP_ROOT_API}api/auth/data`, this.configRequest)
             .then(response => {
                 this.setUserData(response.data.data)
             })
             .catch(err => {
-                console.log("Error fetching category questions", err)
+                console.log("Error fetching", err)
             })
     }
 }
@@ -37,8 +56,32 @@ export default {
 <template>
     <div>
         <NavBar :user-logged-in="true" />
-        <div class="container mt-5 pt-5">
-            <h2>Edit Profil</h2>
+        <div class="container mt-5 pt-5 w-100 d-flex justify-content-start flex-column">
+            <h2 class="my-5">Edit Profile</h2>
+            <form action="#" v-on:submit.prevent="editUser" class="w-25 mx-auto">
+                <!-- alert -->
+                <div class="alert alert-danger alert-dismissible d-flex align-items-center" role="alert"
+                    v-if="messageError">
+                    <i class="bi bi-exclamation-triangle-fill me-2"></i>
+                    <div class="m-0">
+                        {{ messageError }}
+                    </div>
+                </div>
+
+                <div class="form-group mb-3">
+                    <label for="" class="form-label">Username</label>
+                    <input type="text" class="form-control rounded-pill bg-body-secondary" required
+                        v-model="userData.username">
+                </div>
+                <div class="form-group">
+                    <label for="" class="form-label">Email</label>
+                    <input type="email" v-model="userData.email" class="form-control rounded-pill bg-body-secondary"
+                        required>
+                </div>
+                <button type="submit" class="btn btn-dark w-100 rounded-pill mt-5 py-2">Save</button>
+                <button type="button" class="btn bg-body-secondary w-100 rounded-pill mt-3 py-2"
+                    @click="goBack">Cancel</button>
+            </form>
         </div>
     </div>
 </template>
