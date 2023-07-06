117aa4c2aa4510ebf6d557035369d8fa6c1b659a
dhaniarfebrin <dhaniarfebrinwahyudi@gmail.com>
2023-07-06 15:41:39 +0700

    feat: read comments total

--- src/components/QuestionCard.vue
+++ src/components/QuestionCard.vue
@@ -7,19 +7,23 @@ export default {
 
 <template>
     <div class="card bg-transparent border border-0 border-top p-4">
-        <div class="d-flex">
+        <div class="d-flex w-100">
             <div class="img-user rounded-circle">
                 <img src="https://i.pinimg.com/originals/b5/6d/9e/b56d9ed31076329211d42bd8ff340914.jpg" alt="">
             </div>
-            <div class="ms-3 d-flex align-items-center">
-                <span class="fs-5">{{ question.user_id ? question.user_id.username : "{Deleted User}" }}</span>
+            <div class="ms-3 d-flex align-items-center w-100">
+                <div class="d-flex flex-column">
+                    <span class="fs-5">{{ question.user_id ? question.user_id.username : "{Deleted User}" }}</span>
+                    <span class="form-text m-0">{{ question.date_created }} {{ question.time }}</span>
+                </div>
+                <span class="fw-light badge rounded-pill text-bg-secondary ms-auto">{{ question.kategori_id?.kategori }}</span>
             </div>
         </div>
         <p class="mt-3 fw-superlight">{{ question.content }}</p>
         <div class="d-flex">
             <div class="d-flex align-items-center">
-                <span class="fw-light badge rounded-pill text-bg-secondary">{{ question.kategori_id?.kategori }}</span>
-                <span class="ms-2 form-text m-0">{{ question.date_created }} {{ question.time }}</span>
+                <span><i class="bi bi-chat"></i></span>
+                <span class="ms-1 my-0 form-text">{{ question.total_comments }}</span>
             </div>
             <router-link :to="`/question/${question._id}`" class="ms-auto btn btn-outline-dark rounded-pill px-4">See</router-link>
         </div>
