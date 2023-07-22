18b05916e2b7b79420795962b60298c0c78e77e1
dhaniarfebrin <dhaniarfebrinwahyudi@gmail.com>
2023-07-22 19:12:47 +0700

    view: adding user photo icon navbar mobile

--- src/components/NavBarMobile.vue
+++ src/components/NavBarMobile.vue
@@ -1,4 +1,6 @@
 <script>
+import VueJwtDecode from 'vue-jwt-decode'
+
 export default {
     name: 'NavBarMobile',
     data() {
@@ -11,6 +13,13 @@ export default {
         const token = localStorage.getItem('user')
         this.userLoggedIn = token ? true : false
         this.routeProfile = this.userLoggedIn ? "/profile" : "/login"
+
+        if (this.userLoggedIn) {
+            const token = localStorage.getItem("user")
+            const decode = VueJwtDecode.decode(token)
+
+            this.isAdmin = decode.is_admin ? true : false
+        }
     }  
 }
 </script>
@@ -25,11 +34,17 @@ export default {
                 <router-link to="/search" class="d-flex flex-column text-secondary justify-content-center px-1">
                     <i class="bi bi-search"></i>
                 </router-link>
-                <router-link to="/create-question" class="d-flex flex-column text-secondary justify-content-center px-1">
+                <router-link to="/create-question" class="d-flex flex-column text-secondary justify-content-center px-1" v-if="!isAdmin">
                     <i class="bi bi-plus-circle-fill"></i>
                 </router-link>
                 <router-link :to="routeProfile" class="d-flex flex-column text-secondary justify-content-center px-1">
-                    <i class="bi bi-person-fill"></i>
+                    <i class="bi bi-person-fill" v-if="!userLoggedIn"></i>
+                    <div class="d-flex align-items-center justify-content-center" v-else>
+                        <div class="img-user-comment rounded-circle">
+                            <img src="https://i.pinimg.com/originals/b5/6d/9e/b56d9ed31076329211d42bd8ff340914.jpg"
+                                alt="">
+                        </div>
+                    </div>
                 </router-link>
             </div>
         </div>
@@ -54,4 +69,15 @@ a.text-secondary:focus {
     color: #dc3545 !important;
 }
 
+.img-user-comment {
+    width: 22px;
+    height: 22px;
+    overflow: hidden;
+    min-width: 22px;
+}
+
+.img-user-comment>img {
+    width: 100%;
+    height: auto;
+}
 </style>
